dbbd9e91aed39cd11deb9078f4fd196458e13397
Ahmad Siddique <asshikrani66@gmail.com>
2026-02-05 20:43:38 +0500

    Feedo project has been added successfully

---
+++ src/app/project/feedo/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Feedo from '@/components/md/Feedo.mdx'
+import ProjNav from '@/components/elements/ProjNav'
+
+const page = () => {
+  return (
+    <main className='my-10 '>
+        <ProjNav />
+        <Feedo />
+    </main>
+  )
+}
+
+export default page
